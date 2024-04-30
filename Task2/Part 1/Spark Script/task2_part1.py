acbe635965a7dfbd96b8feb2348b574c57fac612
Harsh Malik <94760103+HarshMalik7@users.noreply.github.com>
2024-04-30 03:28:19 +0100

    Add files via upload

---
+++ Task2/Part 1/Spark Script/task2_part1.py
@@ -0,0 +1,61 @@
+import sys, string
+import os
+import socket
+import time
+import operator
+import boto3
+import json
+from pyspark.sql import SparkSession
+from datetime import datetime
+from pyspark.sql.functions import from_unixtime, date_format
+from pyspark.sql.functions import to_date, count, col, concat_ws
+from graphframes import *
+
+
+
+if __name__ == "__main__":
+
+    spark = SparkSession\
+        .builder\
+        .appName("Task2")\
+        .getOrCreate()
+    
+    # shared read-only object bucket containing datasets
+    s3_data_repository_bucket = os.environ['DATA_REPOSITORY_BUCKET']
+
+    s3_endpoint_url = os.environ['S3_ENDPOINT_URL']+':'+os.environ['BUCKET_PORT']
+    s3_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+    s3_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+    s3_bucket = os.environ['BUCKET_NAME']
+
+    hadoopConf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoopConf.set("fs.s3a.endpoint", s3_endpoint_url)
+    hadoopConf.set("fs.s3a.access.key", s3_access_key_id)
+    hadoopConf.set("fs.s3a.secret.key", s3_secret_access_key)
+    hadoopConf.set("fs.s3a.path.style.access", "true")
+    hadoopConf.set("fs.s3a.connection.ssl.enabled", "false")
+
+    rideshare_data = spark.read.options(header=True).csv("s3a://" + s3_data_repository_bucket + "/ECS765/rideshare_2023/rideshare_data.csv")# /read from the bucket file
+
+    rideshare_data = rideshare_data.withColumn('date', from_unixtime(col("date"),"-M")) # Changing UNIX timestamp to just month
+    
+    data = rideshare_data.groupBy('business','date').count() # Grouping by business and date(month)
+    
+    data = data.cache() # Saving the dataframe in cache memory since it will be used again
+    
+    data_uber = data.filter(rideshare_data.business == 'Uber') # Filtering data for Uber
+    data_lyft = data.filter(rideshare_data.business == 'Lyft') # Filtering data for Lyft
+    
+    data_uber = data_uber.select(concat_ws('', data.business, data.date).alias('business-date'), 'count') # Concating business name and month to get the desired labels for Uber
+    data_lyft = data_lyft.select(concat_ws('', data.business, data.date).alias('business-date'), 'count') # Concating business name and month to get the desired labels for Lyft
+
+    # Visualising the dataframe in terminal
+    print("The data for Uber's total business each month:")
+    data_uber.show() 
+    print("The data for Lyft's total business each month:")
+    data_lyft.show()
+    
+    data_uber.coalesce(1).write.option('header', True).csv("s3a://" + s3_bucket + '/Uber-data/') # Saving the dataframe as a single csv file to my personal bucket
+    data_lyft.coalesce(1).write.option('header', True).csv("s3a://" + s3_bucket + '/Lyft-data/') # Saving the dataframe as a single csv file to my personal bucket
+    
+    spark.stop()
